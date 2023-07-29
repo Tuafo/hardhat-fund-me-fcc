0c6a566b9ebe996e2d504ac81575a08b1c46e0b2
Tuafo <thiagomacenogoulart@gmail.com>
2023-07-29 16:10:52 -0300

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,57 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY =
+    process.env.ETHERSCAN_API_KEY || "GU7ZT4B7HHPJTXKJ2EH3195TAR48JIQZHU"
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 31337,
+        },
+    },
+    // solidity: "0.8.20",
+    solidity: {
+        compilers: [{ version: "0.8.20" }, { version: "0.6.8" }],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+        customChains: [],
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        // token: "MATIC",
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+}
